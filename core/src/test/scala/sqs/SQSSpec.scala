9e5e297529de56cac14c2eb1f569fbf89b1a7335
thomas pocreau <thomas.poc@gmail.com>
2015-06-16 14:30:18 +0200

    Adds messageAttributeNames params to receiveMessageAsStream

--- core/src/test/scala/sqs/SQSSpec.scala
+++ core/src/test/scala/sqs/SQSSpec.scala
@@ -4,25 +4,20 @@ package sqs
 import akka.actor._
 import akka.stream._
 import akka.stream.scaladsl._
-
-import com.amazonaws.auth.{BasicAWSCredentials, AWSCredentials, AWSCredentialsProvider}
-import com.amazonaws.auth.profile.ProfileCredentialsProvider
-import com.amazonaws.regions.{Regions, Region}
+import com.amazonaws.regions.{Region, Regions}
 import com.amazonaws.services.sqs.AmazonSQSAsyncClient
-import com.amazonaws.services.sqs.model.{SendMessageRequest, Message, CreateQueueRequest}
-import com.mfglabs.stream.{FlowExt, SinkExt}
+import com.amazonaws.services.sqs.model.{CreateQueueRequest, MessageAttributeValue, SendMessageRequest}
 import com.github.dwhjames.awswrap.sqs.AmazonSQSScalaClient
+import com.mfglabs.stream.SinkExt
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.time.{Millis, Seconds, Span}
+import org.scalatest.{FlatSpec, Matchers}
 
-import org.scalatest.concurrent.{AsyncAssertions, ScalaFutures}
-import org.scalatest.time.{Seconds, Millis, Minutes, Span}
-import org.scalatest.{Matchers, FlatSpec}
-
-import scala.concurrent._
 import scala.concurrent.duration._
-
-import scala.util.{Try, Random}
+import scala.util.Random
 
 class SQSSpec extends FlatSpec with Matchers with ScalaFutures {
+
   import scala.collection.JavaConversions._
 
   implicit override val patienceConfig =
@@ -31,13 +26,13 @@ class SQSSpec extends FlatSpec with Matchers with ScalaFutures {
   implicit val as = ActorSystem()
   implicit val fm = ActorFlowMaterializer()
 
-  import scala.concurrent.ExecutionContext.Implicits.global
-
   val sqs = new AmazonSQSScalaClient(new AmazonSQSAsyncClient(), scala.concurrent.ExecutionContext.Implicits.global)
   val builder = SQSStreamBuilder(sqs)
 
   val testQueueName = "commons-aws-sqs-test-" + Random.nextInt()
   val testQueueName2 = "commons-aws-sqs-test-" + Random.nextInt()
+
+  val messageAttributeKey = "attribute_key"
 
   "SQS client" should "send message and receive them as streams" in {
     sqs.client.setRegion(Region.getRegion(Regions.EU_WEST_1))
@@ -51,19 +46,21 @@ class SQSSpec extends FlatSpec with Matchers with ScalaFutures {
     val msgs = for (i <- 1 to 200) yield s"Message $i"
     val futSent = Source(msgs)
       .map { body =>
-        val req = new SendMessageRequest()
-        req.setMessageBody(body)
-        req.setQueueUrl(queueUrl)
-        req
-      }
+      val req = new SendMessageRequest()
+      req.addMessageAttributesEntry(messageAttributeKey, new MessageAttributeValue().withDataType("String").withStringValue(body))
+      req.setMessageBody(body)
+      req.setQueueUrl(queueUrl)
+      req
+    }
       .via(builder.sendMessageAsStream())
       .take(200)
       .runWith(SinkExt.collect)
-    val futReceived = builder.receiveMessageAsStream(queueUrl, autoAck = true).take(200).runWith(SinkExt.collect)
+    val futReceived = builder.receiveMessageAsStream(queueUrl, autoAck = true, messageAttributeNames = List(messageAttributeKey)).take(200).runWith(SinkExt.collect)
 
     val (sent, received) = futSent.zip(futReceived).futureValue
 
     received.map(_.getBody).sorted shouldEqual msgs.sorted
+    received.map(_.getMessageAttributes.get(messageAttributeKey).getStringValue).sorted shouldEqual msgs.sorted
 
     // testing auto-ack (queue must be empty)
     val res = builder
